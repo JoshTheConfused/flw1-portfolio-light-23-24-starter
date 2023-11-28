616475e32226b37460c51c697199f5f70fca43b7
JoshTheConfused <97866996+JoshTheConfused@users.noreply.github.com>
2023-11-28 18:50:13 +0000

    Continued carousel work, put JSON in js file

--- carousel.js
+++ carousel.js
@@ -3,11 +3,35 @@ let right = document.getElementById('right');
 let description = document.getElementById('activity-description');
 let image = document.getElementById('activity-image');
 let curSlide = 0;
-let slideData = fetch("carousel.json");
+let slideData = [
+  {
+    description: "###",
+    imgSrc: "images/###.png"
+  },
+  {
+    description: "%%%",
+    imgSrc: "images/%%%.png"
+  }
+]
 
-console.log(slideData);
-
-for (let i in slideData) {
-  console.log(i.description);
-  console.log(i.imgSrc);
+function loadSlideData(slideIndex) {
+  newDescription = slideData[slideIndex].description;
+  description.innerHTML = newDescription;
+  //image.src = slideData[slideIndex].imgSrc
 }
+
+loadSlideData(curSlide);
+left.addEventHandler("click", function() {
+  curSlide--;
+  if (curSlide < 0) {
+    curSlide = slideData.length - 1;
+  }
+  loadSlideData(curSlide);
+});
+right.addEventHandler("click", function() {
+  curSlide++;
+  if (curSlide > slideData.length - 1) {
+    curSlide = 0;
+  }
+  loadSlideData(curSlide);
+});
