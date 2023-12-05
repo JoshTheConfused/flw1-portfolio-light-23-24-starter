315a13e1bc52de3511d75a88f7b6a4e3f2c6033b
JoshTheConfused <97866996+JoshTheConfused@users.noreply.github.com>
2023-12-05 03:31:58 +0000

    Added Wave Function Collapse to about page

---
+++ WaveFunctionCollapse/aboutWFC.js
@@ -0,0 +1,87 @@
+/*Wave Function Collapse (tiled model)
+All Credit for this implementation goes to The Coding Train / Daniel Shiffman: https://www.youtube.com/watch?v=rI_y2GAlQFM
+This was coded almost exactly to his implementation as shown in the above video
+Original Wave Function Collapse: https://github.com/mxgmn/WaveFunctionCollapse
+
+This tileset (knots) was taken from the original WFC github*/
+
+const tileImages = [];
+const tiles = [];
+const grid = [];
+let collapsed = [];
+let uncollapsed = [];
+const cellSize = 30; //Cells are always 30x30 pixels.
+
+//Canvas dimensions are set each time the page is loaded.
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+//Page is filled, final row and column of tiles may be clipped.
+const gridWidth = Math.floor((w / cellSize) + 1);
+const gridHeight = Math.floor((h / cellSize) + 1);
+
+const gridSpaceTotal = gridWidth * gridHeight; //Total number of cells
+let WFC;
+
+function preload() { //Load all tile images into an array
+  tileImages.push(loadImage("WaveFunctionCollapse/images/About/corner.png"));
+  tileImages.push(loadImage("WaveFunctionCollapse/images/About/empty.png"));
+  tileImages.push(loadImage("WaveFunctionCollapse/images/About/cross.png"));
+  tileImages.push(loadImage("WaveFunctionCollapse/images/About/line.png"));
+  tileImages.push(loadImage("WaveFunctionCollapse/images/About/t.png"));
+}
+
+function setup() {
+  WFC = createCanvas(w, h);
+  WFC.parent("#WFCBackground"); //Attach canvas to div containing whole home page
+
+  //Create tiles array containing every tile
+  tiles.push(new Tile(tileImages[0], ["1", "1", "0", "0"]));
+  tiles.push(new Tile(tileImages[1], ["0", "0", "0", "0"]));
+  tiles.push(new Tile(tileImages[2], ["1", "1", "1", "1"]));
+  tiles.push(new Tile(tileImages[3], ["0", "1", "0", "1"]));
+  tiles.push(new Tile(tileImages[4], ["0", "1", "1", "1"]));
+
+  //Adds every rotated tile
+  const stop = tiles.length - 1;
+  for (let i = 0; i < stop; i++) {
+    for(let j = 1; j < 4; j++) {
+      tiles.push(tiles[i].rotate(j));
+    }
+  }
+
+  //Fill grid with new cells
+  for (let i = 0; i < gridSpaceTotal; i++) {
+    let cell = new Cell(tiles.length, i % gridWidth, Math.floor(i / gridWidth));
+    grid[i] = cell;
+    uncollapsed.push(cell);
+  }
+
+  //Generate tile adjacency rules
+  for (let i = 0; i < tiles.length; i++) {
+    tiles[i].analyze(tiles);
+  }
+}
+
+function draw() {
+  background(147, 196, 125); //Same as background for whole site
+
+  for (let i = 0; i < collapsed.length; i++) { //Draw every collapsed cell
+    let cell = collapsed[i];
+    if (cell.options[0] != 1) {
+      let index = cell.options[0];
+      image(tiles[index].img, cell.x * cellSize, cell.y * cellSize, cellSize, cellSize);
+    }
+  }
+
+  waveFunctionCollapse(collapsed, uncollapsed, grid, gridWidth, gridHeight);
+}
+
+function checkValid(allOptions, validOptions) {
+  //Intersection of the two arrays, removes values from 'allOptions' which are not in 'valid Options'
+  for (let i = allOptions.length - 1; i >= 0; i--) {
+    if (!validOptions.includes(allOptions[i])) {
+      allOptions.splice(i, 1);
+    }
+  }
+}
