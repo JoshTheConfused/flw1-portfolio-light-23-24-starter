597d4010e3e12f82ff344a21fb4ada36480f0106
JoshTheConfused <97866996+JoshTheConfused@users.noreply.github.com>
2023-12-05 02:06:32 +0000

    Added activity descriptions

--- carousel.js
+++ carousel.js
@@ -7,23 +7,23 @@ let activityImage = document.getElementById("activity-image");
 let curSlide = 0;
 
 let shakespeare = {
-  description: "Shakespeare",
+  description: "For four summers, I had the opportunity to be a part of the West Kortright Center's Shakespeare in the Valley summer acting program, located in upstate New York. I have performed in <span class=\"italics\">Macbeth, 12th night, The Winter's Tale, </span> and <span class=\"italics\">Julius Caesar</span> there. In addition, I previously performed in <span class=\"italics\">The Tempest, Henry V, Hamlet, </span> and <span class=\"italics\">As You Like It</span> with the Brooklyn Shakespeare Company.",
   imgSrc: "shakespeare"
 };
 let frisbee = {
-  description: "Frisbee",
+  description: "Since 10th grade, I have played Ultimate Frisbee with the BHSEC-M Bardbarians. We travel to several tournaments throughout the year (one as far as Virginia), at which we've been able to compete against some of the best teams in the country, including Columbia High School, where the sport was invented. We also participate in the city regular season and playoffs during the spring.",
   imgSrc: "frisbee"
 };
 let poetry = {
-  description: "Poetry Club",
+  description: "At Bard, I am an avid participant in our Poetry Club, and have been since 10th grade, when two of my friends were given charge of the club. We use the club as a space to read, write, and listen to poetry, and we publish a monthly zine, as well as an inter-school zine at the end of the year. Around the holidays, we have our annual Secret Sonnet event (which I proudly take credit for naming).",
   imgSrc: "poetry"
 };
 let skiing = {
-  description: "Shredding",
+  description: "I've been skiing for about as long as I've been walking. My father grew up in the Adirondacks, near Whiteface Mountain, and my mother took many trips around the country to ski growing up. As a result, they began teaching me from a very young age. I am now a relatively advanced skiier (although I have little experience outside of the East Coast), and a co-leader of my school's Snow Sports Club.",
   imgSrc: "skiing"
 };
 let computers = {
-  description: "Building my computer",
+  description: "During quarantine, I began researching computer hardware and PC building, and in late 2020 I built myself a computer. Since then, I've mainly used it for coding and the typical internet browsing, along with some video games when I've had the chance to play them. Almost three years later, I am still using the same one, and it's remained functional and efficient the whole time.",
   imgSrc: "computer"
 };
 
